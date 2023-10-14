7e4032417fdb2a6ee34f92fa93a6ab55bfa8f95c
GustavoSVasconcelos <129122651+GustavoSVasconcelos@users.noreply.github.com>
2023-10-14 16:17:54 -0400

    criado telas de cadastro/tela de login/ tela de rec. de senha

---
+++ login.js
@@ -0,0 +1,21 @@
+// Função para verificar o login e redirecionar
+function logar() {
+    var email = document.getElementById("email").value;
+    var senha = document.getElementById("senha").value;
+
+    // Obter os dados de cadastro do localStorage
+    var usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+    // Verificar se o email e a senha correspondem a um usuário
+    var usuario = usuarios.find(function(usuario) {
+        return usuario.email === email && usuario.senha === senha;
+    });
+
+    if (usuario) {
+        // Login com sucesso, redirecione para teste.html
+        alert("Sucesso!");
+        window.location.href = "teste.html";
+    } else {
+        alert("Login falhou. Verifique seu email e senha.");
+    }
+}
